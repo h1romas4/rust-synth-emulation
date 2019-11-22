0c9a13e9a3bd7175c1d894d19e70af1593c803fa
h1romas4 <h1romas4@gmail.com>
2019-11-23 03:18:12 +0900

    add eslintrc

---
+++ wasm-synth-player/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    }
+};
