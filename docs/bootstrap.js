cc757f4504be2e80d85bee0a7c2ff9553c6b93a2
h1romas4 <h1romas4@gmail.com>
2019-12-14 11:49:56 +0900

    update package

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -171,7 +171,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/wasm_vgm_player_bg.wasm":"ab99a045cb64673c236f"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/wasm_vgm_player_bg.wasm":"0eaee8ad3e3c7f8bda8c"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
